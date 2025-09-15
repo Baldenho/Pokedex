9999ef4eac5312aa8c8e8359eec79dec6ae2767f
Baldenho <gameshowdonw@gmail.com>
2025-09-15 20:44:53 -0300

    Estudo de C# com database de pokedex

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Pokedex.Models;
+using Pokedex.Data;
+
+namespace Pokedex.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    private readonly AppDbContext _db;
+
+    public HomeController(ILogger<HomeController> logger, appDbContext db)
+    {
+        _logger = logger;
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        var pokemons = _db.Pokemons
+            .Include(p => p.Tipos)
+            .ThenInclude(pt => pt.Tipo)
+            .ToList();
+        return View(pokemons);
+
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
